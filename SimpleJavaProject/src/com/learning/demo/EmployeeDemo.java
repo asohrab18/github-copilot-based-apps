15f5c426f64c282348ba8e9495c029e4688cc0cf
Sohrab Alam <sohrab.alam86@gmail.com>
2026-09-15 12:39:09 +0530

    Refactor employee output into reusable method

--- SimpleJavaProject/src/com/learning/demo/EmployeeDemo.java
+++ SimpleJavaProject/src/com/learning/demo/EmployeeDemo.java
@@ -8,6 +8,10 @@ public class EmployeeDemo {
 
 		Employee employee = new Employee(101, "Adam Smith", 75000.00);
 
+		printEmployeeDetails(employee);
+	}
+
+	public static void printEmployeeDetails(Employee employee) {
 		System.out.println("Employee ID: " + employee.getEmployeeId());
 		System.out.println("Employee Name: " + employee.getEmployeeName());
 		System.out.println("Salary: " + employee.getSalary());
