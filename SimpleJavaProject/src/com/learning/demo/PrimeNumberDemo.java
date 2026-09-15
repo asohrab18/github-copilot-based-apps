2e8b0e56140bf47fbd7667fa7b5d940e19038213
Sohrab Alam <sohrab.alam86@gmail.com>
2026-09-15 19:39:01 +0530

    refactor: simplify Employee property names

--- SimpleJavaProject/src/com/learning/demo/PrimeNumberDemo.java
+++ SimpleJavaProject/src/com/learning/demo/PrimeNumberDemo.java
@@ -5,10 +5,10 @@ import com.learning.utils.AppUtils;
 public class PrimeNumberDemo {
 
 	public static void main(String[] args) {
-		int[] testNumbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+		final int[] sampleNumbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 
-		for (int number : testNumbers) {
-			System.out.println(number + " is prime: " + AppUtils.isPrime(number));
+		for (int number : sampleNumbers) {
+			System.out.printf("%d is prime: %b%n", number, AppUtils.isPrime(number));
 		}
 	}
 }
