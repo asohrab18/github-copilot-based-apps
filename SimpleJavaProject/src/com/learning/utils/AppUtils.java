bcfd6eb40df577c356464f4736e654479cea5b75
Sohrab Alam <sohrab.alam86@gmail.com>
2026-09-16 14:45:09 +0530

    methods  updated

--- SimpleJavaProject/src/com/learning/utils/AppUtils.java
+++ SimpleJavaProject/src/com/learning/utils/AppUtils.java
@@ -2,52 +2,52 @@ package com.learning.utils;
 
 public final class AppUtils {
 
-	private AppUtils() {
-	}
+    private AppUtils() {
+    }
 
-	/**To check whether a number is prime or not.*/
-	public static boolean isPrime(int number) {
-		if (number <= 1) {
-			return false;
-		}
-		if (number <= 3) {
-			return true;
-		}
-		if (number % 2 == 0) {
-			return false;
-		}
+    /**To check whether a number is prime or not.*/
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
 
-		for (int divisor = 3; divisor <= Math.sqrt(number); divisor += 2) {
-			if (number % divisor == 0) {
-				return false;
-			}
-		}
+        for (int divisor = 3; divisor <= Math.sqrt(number); divisor += 2) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
 
-		return true;
-	}
-	
-	/**To find factorial of a number.*/
-	public static long calculateFactorial(int number) {
-		validateNonNegative(number);
+        return true;
+    }
+    
+    /**To find factorial of a number.*/
+    public static long calculateFactorial(int number) {
+        validateNonNegative(number);
 
-		long result = 1;
-		for (int current = 2; current <= number; current++) {
-			result *= current;
-		}
-		
-		return result;
-	}
+        long result = 1;
+        for (int current = 2; current <= number; current++) {
+            result *= current;
+        }
+        
+        return result;
+    }
 
-	private static void validateNonNegative(int number) {
-		if (number < 0) {
-			throw new IllegalArgumentException("Number must be non-negative.");
-		}
-	}
+    private static void validateNonNegative(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number must be non-negative.");
+        }
+    }
 
-	public static void printFactorials(int[] numbers) {
-		for (int number : numbers) {
-			long factorial = calculateFactorial(number);
-			System.out.println(number + "! = " + factorial);
-		}
-	}
+    public static void printFactorials(int[] numbers) {
+        for (int number : numbers) {
+            long factorial = calculateFactorial(number);
+            System.out.println(number + "! = " + factorial);
+        }
+    }
 }
